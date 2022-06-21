9d67cb6f801cb5dacc5ee95b259ef52a9cf71738
Laura-Ruiz <laaura.26@gmail.com>
2022-06-21 18:19:22 +0200

    [Exercise1]

---
+++ src/components/escena/escena.js
@@ -0,0 +1,15 @@
+
+
+function Escena() {
+    return (
+        <div>
+            <p>
+                El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial.
+            </p>
+        </div>
+    )
+
+
+}
+
+export default Escena;
